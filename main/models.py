cd54b05ec856beb76efc55b255bd17ac15134924
csAfter40 <csafter40@gmail.com>
2022-03-11 17:30:16 +0300

    UserProfile model created, migrations are applied

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
+def upload_to(instance, filename):
+    return f'profile_pictures/{filename}'
 
 class User(AbstractUser):
     
@@ -9,6 +12,7 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
 
 class Currency(models.Model):
     code = models.CharField(max_length=2)
@@ -36,3 +40,20 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+
+class UserProfile(models.Model):
+    gender_choices = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    gender = models.CharField(max_length=1, choices=gender_choices)
+    date_of_birth = models.DateField(null=True, blank=True)
+    nationality = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    phone_number = PhoneNumberField()
+    profile_picture = models.ImageField('Profile Picture', upload_to=upload_to, default='profile_pictures/default.jpg')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
